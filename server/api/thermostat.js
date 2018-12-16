1b376aaa8896360b664cdfd3cc5dca603fbd5609
Mary Warrick <mewarrick@gmail.com>
2018-12-16 16:23:56 -0600

    post temp and get config

--- server/api/thermostat.js
+++ server/api/thermostat.js
@@ -1,17 +1,22 @@
 const router = require('express').Router()
-const { Temperature } = require('../db/models')
+const { Temperature, Config } = require('../db/models')
 module.exports = router
 
 router.post('/temperature', async (req, res, next) => {
   try {
       const {temperature} = req.body
       const newTemp = Temperature.create({temperature})
-
-      //temperature
+      res.json(204)
   } catch (err) {
     next(err)
   }
 })
 
-//
-
+router.get('/config/:id', async (req, res, next) => {
+  try {
+    const thermostatConfig = Config.findOne(req.params.id)
+    res.json(thermostatConfig)
+  }catch (err) {
+    next(err)
+  }
+})
