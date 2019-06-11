d8c8116bf006e1ac0f38845bf28c88a336797948
AndrewTrahan <globalART19@gmail.com>
2019-06-11 15:13:51 -0500

    Added schedule db. resetting db in heroku

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,16 +1,11 @@
 const User = require('./user')
 const Temperature = require('./temperature')
+const Schedule = require('./schedule')
 const ThermostatConfig = require('./thermostat-config')
-
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
 
 module.exports = {
   User,
   Temperature,
+  Schedule,
   ThermostatConfig
 }
