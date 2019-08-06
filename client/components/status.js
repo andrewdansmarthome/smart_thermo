98da52947904581836ad09921927c53676f4873e
Dan Gutt <dgutt10@gmail.com>
2019-08-06 09:31:27 -0500

    Added some stuff to the UI

--- client/components/status.js
+++ client/components/status.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
 const Status = (props) => {
-  return (<div className="status content-panel">Status</div>)
+  return (
+  <div>
+    <div className="status content-panel">
+      <h2 className="content-header">Status</h2>
+      <div>
+        Temperature:
+      </div>
+      <br></br>
+      <div>
+        Status:
+      </div>
+      <br></br>
+      <div>
+        Last on:
+      </div>
+      <br></br>
+    </div>
+  </div>
+  )
 }
 
 export default Status
