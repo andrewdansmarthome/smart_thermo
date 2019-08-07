f57e311dd01428e773255576e8a07f7db7339fba
AndrewTrahan <globalART19@gmail.com>
2019-08-06 22:40:19 -0500

    finished up the footer and styling

--- client/components/footer.js
+++ client/components/footer.js
@@ -5,13 +5,25 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Footer = () => (
-  <div>
-    <hr />
-    <div className="footer-panel">
-      <div className="footer-note">
-        <p>{"Andrew & Dan are badasses. Check out their circuit and coding journey on this project"}</p>
-        <a href="" className="inline-link">HERE</a>
-        <p>.</p>
+  <div className="content-panel">
+    <div className="footer-note">
+      <p>{"Andrew & Dan are badasses. Check out their circuit and coding journey on this project"}</p>
+      <a href="https://github.com/andrewdansmarthome/smart_thermo" className="inline-link">HERE</a>
+      <p>.</p>
+    </div>
+    <div className="footer-info">
+      <h3>Developed by</h3>
+      <div className="dev-section">
+        <div className="dev-info">
+          <h4>Andrew Trahan</h4>
+          <a href="https://github.com/globalart19">GitHub</a>
+          <a href="https://linkedin.com/andrewtrahan">LinkedIn</a>
+        </div>
+        <div className="dev-info">
+          <h4>Dan Gutt</h4>
+          <a href="https://github.com/">GitHub</a>
+          <a href="https://linkedin.com/">LinkedIn</a>
+        </div>
       </div>
     </div>
   </div>
