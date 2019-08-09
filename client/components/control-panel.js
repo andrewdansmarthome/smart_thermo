526ffa24e082e42f5d278294e9ee6982e8dde6b7
AndrewTrahan <globalART19@gmail.com>
2019-08-08 20:27:14 -0500

    started building slider toggle

--- client/components/control-panel.js
+++ client/components/control-panel.js
@@ -4,6 +4,10 @@ const ControlPanel = (props) => {
   return (
     <div className="control-panel content-panel">
       <h2 className="content-header">Control Panel</h2>
+      <label className="toggle-control">
+        <input type="checkbox"></input>
+        <span className="toggle-slider"></span>
+      </label>
     </div>
   )
 }
