e80ec58fe08d341cb9062fdf5a4f5c38e9681d15
AndrewTrahan <globalART19@gmail.com>
2019-08-17 10:55:59 -0500

    made schedule table scrollable

--- client/components/scheduler.js
+++ client/components/scheduler.js
@@ -52,51 +52,53 @@ class Scheduler extends Component {
     return (
       <div className="scheduler content-panel">
         <h2 className="content-header">Scheduler</h2>
-        <table className="schedule-table">
-          <thead>
-            <tr>
-              <th>Day</th>
-              <th>Time</th>
-              <th>Temp</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <ScheduleFormRow
-              key="add-row-key"
-              type="add"
-              submitButtonName="Add"
-              clearButtonName="Clear"
-              handleSubmitRow={this.handleSubmitRow}
-              handleClearRow={this.handleClearRow}
-            />
-            {
-              this.props.schedule.map((scheduleInfo, index) => {
-                return this.state.editRowIndex === index ? (
-                  <ScheduleFormRow
-                    key={`scheduler-edit-row-${index}`}
-                    type="edit"
-                    index={index}
-                    schedule={this.state.editRow}
-                    submitButtonName="Update"
-                    clearButtonName="Cancel"
-                    handleSubmitRow={this.handleSubmitRow}
-                    handleClearRow={this.handleClearRow}
-                  />
-                ) : (
-                  <tr key={`scheduler-row-${index}`}>
-                    <td key={`scheduler-cell-day-${index}`}>{scheduleInfo.day}</td>
-                    <td key={`scheduler-cell-time-${index}`}>{scheduleInfo.time}</td>
-                    <td key={`scheduler-cell-temp-${index}`}>{scheduleInfo.temp}</td>
-                    <td key={`scheduler-cell-action-${index}`}>
-                        <button id={`edit-schedule-${index}`} type='button' className="button secondary" onClick={this.editRow}>Edit</button>
-                    </td>
-                  </tr>
-                );
-              })
-            }
-          </tbody>
-        </table>
+        <div className="schedule-table">
+          <table>
+            <thead>
+              <tr>
+                <th>Day</th>
+                <th>Time</th>
+                <th>Temp</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <ScheduleFormRow
+                key="add-row-key"
+                type="add"
+                submitButtonName="Add"
+                clearButtonName="Clear"
+                handleSubmitRow={this.handleSubmitRow}
+                handleClearRow={this.handleClearRow}
+              />
+              {
+                this.props.schedule.map((scheduleInfo, index) => {
+                  return this.state.editRowIndex === index ? (
+                    <ScheduleFormRow
+                      key={`scheduler-edit-row-${index}`}
+                      type="edit"
+                      index={index}
+                      schedule={this.state.editRow}
+                      submitButtonName="Update"
+                      clearButtonName="Cancel"
+                      handleSubmitRow={this.handleSubmitRow}
+                      handleClearRow={this.handleClearRow}
+                    />
+                  ) : (
+                    <tr key={`scheduler-row-${index}`}>
+                      <td key={`scheduler-cell-day-${index}`}>{scheduleInfo.day}</td>
+                      <td key={`scheduler-cell-time-${index}`}>{scheduleInfo.time}</td>
+                      <td key={`scheduler-cell-temp-${index}`}>{scheduleInfo.temp}</td>
+                      <td key={`scheduler-cell-action-${index}`}>
+                          <button id={`edit-schedule-${index}`} type='button' className="button secondary" onClick={this.editRow}>Edit</button>
+                      </td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
